7d048b71540ba436a59e049cc5596adacfa87bfd
fuliangliang <fuliangliang@oppo.com>
2022-01-22 08:20:01 +0800

    add problem 6 solution

--- src/leetcode/mod.rs
+++ src/leetcode/mod.rs
@@ -3,3 +3,4 @@ pub mod p0001_two_sum;
 pub mod p0002_add_two_nums;
 pub mod p0003_longest_substring_without_repeating_characters;
 pub mod p0005_longest_palindromic_substring;
+pub mod p0006_zigzag_conversion;
---
+++ src/leetcode/p0006_zigzag_conversion.rs
@@ -0,0 +1,54 @@
+struct Solution;
+
+impl Solution {
+    pub fn convert(s: String, num_rows: i32) -> String {
+        if num_rows < 2 {
+            return s;
+        }
+
+        let rows = num_rows as usize;
+        let s: Vec<char> = s.chars().collect();
+        let mut str_vec = vec!["".to_string(); rows];
+
+        let period = rows * 2 - 2;
+        
+        for i in 0..s.len() {
+            let pmod = i % period;
+            if pmod < rows {
+                str_vec[pmod].push(s[i]);
+            } else {
+                str_vec[period - pmod].push(s[i]);
+            }
+        }
+        str_vec.join("")
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    pub fn test_case1() {
+        let s = "PAYPALISHIRING".to_string();
+        let num_rows = 3;
+        let result = Solution::convert(s, num_rows);
+        assert_eq!(result, "PAHNAPLSIIGYIR");
+    }
+
+    #[test]
+    pub fn test_case2() {
+        let s = "PAYPALISHIRING".to_string();
+        let num_rows = 4;
+        let result = Solution::convert(s, num_rows);
+        assert_eq!(result, "PINALSIGYAHRPI");
+    }
+
+    #[test]
+    pub fn test_case3() {
+        let s = "A".to_string();
+        let num_rows = 1;
+        let result = Solution::convert(s, num_rows);
+        assert_eq!(result, "A");
+    }
+}
